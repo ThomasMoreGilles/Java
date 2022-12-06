e2622680cc98cb3c71a894c4fbac1c1224e3e996
gilles20393 <gilles1996@outlook.com>
2022-12-06 14:52:17 +0100

    eval01 oef02

--- Evaluatie/Evaluatie01/oop-test1-test1-start/src/Staff.java
+++ Evaluatie/Evaluatie01/oop-test1-test1-start/src/Staff.java
@@ -40,4 +40,25 @@ public class Staff {
         String output = obj.getRole() + " : " + obj.getName() + "(" + obj.getAge() + ")";
         return output;
     }
+
+    public String getInitials() {
+        Staff obj = new Staff();
+        obj.setName("Roberto Martinez");
+        String fullName = obj.getName();
+
+        String initials = "";
+
+        String[] a = fullName.split(" ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = a[i].substring(0, 1);
+            if (a[i] == a[i].toUpperCase()) {
+                initials += a[i] + ".";
+            }
+        }
+        return initials;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
