f8743c4d4d273c51775fe15c48983826a00ab748
gilles20393 <gilles1996@outlook.com>
2022-12-06 14:12:34 +0100

    eval01 oef01

---
+++ Evaluatie/Evaluatie01/oop-test1-test1-start/src/Staff.java
@@ -0,0 +1,44 @@
+
+public class Staff {
+    private String name;
+    private String role;
+    private Integer age;
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    // Setter
+    public void setName(String N) {
+        this.name = N;
+    }
+
+    public void setRole(String R) {
+        this.role = R;
+    }
+
+    public void setAge(Integer A) {
+        this.age = A;
+    }
+
+    public String toString(String[] args) {
+        Staff obj = new Staff();
+
+        obj.setName("Roberto Martinez");
+        obj.setRole("trainer");
+        obj.setAge(49);
+
+        String output = obj.getRole() + " : " + obj.getName() + "(" + obj.getAge() + ")";
+
+        return output;
+    }
+}
