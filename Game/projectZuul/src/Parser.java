42adefd650fa6562a8d04b6a56f9915663f6362c
gilles20393 <gilles1996@outlook.com>
2023-01-27 17:47:52 +0100

    switch enhanced

--- Game/projectZuul/src/Parser.java
+++ Game/projectZuul/src/Parser.java
@@ -1,41 +1,38 @@
 import java.util.Scanner;
 
 /**
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- * 
+ * This class is part of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.
+ * <p>
  * This parser reads user input and tries to interpret it as an "Adventure"
  * command. Every time it is called it reads a line from the terminal and
  * tries to interpret the line as a two word command. It returns the command
  * as an object of class Command.
- *
+ * <p>
  * The parser has a set of known command words. It checks user input against
  * the known commands, and if the input is not one of the known commands, it
  * returns a command object that is marked as an unknown command.
- * 
- * @author  Michael Kölling and David J. Barnes
+ *
+ * @author Michael Kölling and David J. Barnes
  * @version 2011.07.31
  */
 
-public class Parser
-{
-    private CommandWords commands;  // holds all valid command words
-    private Scanner reader;         // source of command input
+public class Parser {
+    private final CommandWords commands;  // holds all valid command words
+    private final Scanner reader;         // source of command input
 
     /**
      * Create a parser to read from the terminal window.
      */
-    public Parser() 
-    {
-        commands = new AdventureCommandWords ();
+    public Parser() {
+        commands = new AdventureCommandWords();
         reader = new Scanner(System.in);
     }
 
     /**
      * @return The next command from the user.
      */
-    public Command getCommand() 
-    {
+    public Command getCommand() {
         String inputLine;   // will hold the full input line
         String word1 = null;
         String word2 = null;
@@ -46,9 +43,9 @@ public class Parser
 
         // Find up to two words on the line.
         Scanner tokenizer = new Scanner(inputLine);
-        if(tokenizer.hasNext()) {
+        if (tokenizer.hasNext()) {
             word1 = tokenizer.next();      // get first word
-            if(tokenizer.hasNext()) {
+            if (tokenizer.hasNext()) {
                 word2 = tokenizer.next();      // get second word
                 // note: we just ignore the rest of the input line.
             }
